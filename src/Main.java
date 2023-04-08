0c2209357929c4071cc29223ddb4e911c4699037
Dmitry Kanger <dmitrykanger@yandex.ru>
2023-04-09 00:07:18 +0300

    second Fixed

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,22 @@ public class Main {
         double yearBet = 9.99;
         //  double creditPeriod = 12;
 
-        double payOne = service.calc(creditSumm, yearBet, 12);
-        int oneYear = (int) payOne;
+        double oneYear = service.calc(creditSumm, yearBet, 12);
+        int payOneYear = (int) oneYear;
         System.out.println("При сумме кредита в " + creditSumm + " рублей, сроком на 12 месяцев (1 год), и процентной ставке " + yearBet + "%, ");
-        System.out.println("ежемесячный платёж составит " + oneYear + " рублей.");
+        System.out.println("ежемесячный платёж составит " + payOneYear + " рублей.");
         System.out.println();
 
-        double payTwo = service.calc(creditSumm, yearBet, 24);
-        int twoYears = (int) payTwo;
+        double twoYears = service.calc(creditSumm, yearBet, 24);
+        int payTwoYears = (int) twoYears;
         System.out.println("При сумме кредита в " + creditSumm + " рублей, сроком на 24 месяца (2 года), и процентной ставке " + yearBet + "%, ");
-        System.out.println("ежемесячный платёж составит " + twoYears + " рублей.");
+        System.out.println("ежемесячный платёж составит " + payTwoYears + " рублей.");
         System.out.println();
 
-        double payThree = service.calc(creditSumm, yearBet, 36);
-        int threeYears = (int) payThree;
+        double threeYears = service.calc(creditSumm, yearBet, 36);
+        int payThreeYears = (int) threeYears;
         System.out.println("При сумме кредита в " + creditSumm + " рублей, сроком на 36 месяцев (3 года), и процентной ставке " + yearBet + "%, ");
-        System.out.println("ежемесячный платёж составит " + threeYears + " рубля.");
+        System.out.println("ежемесячный платёж составит " + payThreeYears + " рубля.");
 
 
     }
